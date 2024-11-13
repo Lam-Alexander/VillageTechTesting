a086c0048a69bff7c4fff18b96f7553d4db23e9a
Alex <Lam.Alexander@hotmail.com>
2024-11-13 15:57:22 -0700

    Updated Test code

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  transform: {
+    "^.+\\.jsx?$": "babel-jest", // This tells Jest to use Babel to transform JavaScript and JSX files
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!your-package-to-transform|other-packages).+\\.js$", // Optional: Only transform specific node_modules packages if necessary
+  ],
+  moduleNameMapper: {
+    "\\.css$": "identity-obj-proxy", // Mock CSS imports
+  },
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "./reports", outputName: "junit.xml" }], // Add JUnit reporter
+  ],
+  testEnvironment: "jsdom",  // Ensure this line is added to use the jsdom environment for React testing
+};
