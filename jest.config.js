c535484fa76f985a57723fcff15570eab5999fbc
Alex <Lam.Alexander@hotmail.com>
2024-11-13 19:16:36 -0700

    Please save me 123

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,24 @@
 module.exports = {
   transform: {
-    "^.+\\.jsx?$": "babel-jest", // This tells Jest to use Babel to transform JavaScript and JSX files
+    "^.+\\.jsx?$": "babel-jest",
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!your-package-to-transform|other-packages).+\\.js$", // Optional: Only transform specific node_modules packages if necessary
+    "/node_modules/(?!your-package-to-transform|other-packages).+\\.js$",
   ],
   moduleNameMapper: {
-    "\\.css$": "identity-obj-proxy", // Mock CSS imports
+    "\\.css$": "identity-obj-proxy",
   },
   reporters: [
     "default",
-    ["jest-junit", { outputDirectory: "./reports", outputName: "junit.xml" }], // Add JUnit reporter
+    ["jest-junit", { outputDirectory: "./reports", outputName: "junit.xml" }],
   ],
-  testEnvironment: "jsdom",  // Ensure this line is added to use the jsdom environment for React testing
-  collectCoverage: true,  // Enable coverage collection
-  coverageDirectory: "./coverage",  // Specify the directory where Jest should output coverage files
-  coverageReporters: ["text", "lcov", "json", "cobertura"], // Choose the format for the coverage report
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "client/src/**/*.{js,jsx}", // Adjust this path to target your source files
+    "!**/node_modules/**",
+    "!**/vendor/**",
+  ],
   coverageThreshold: {
     global: {
       branches: 80,
