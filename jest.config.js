cf7ebb5a4679705baaee461bad63b94bc1e85ebb
Alex <Lam.Alexander@hotmail.com>
2024-11-14 13:23:23 -0700

    please update

--- jest.config.js
+++ jest.config.js
@@ -16,9 +16,5 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "./client/coverage", // Change this line to save coverage inside the client/coverage folder
   coverageReporters: ["text", "lcov", "json", "cobertura"], // Choose the format for the coverage report
-  collectCoverageFrom: [
-    "src/**/*.{js,jsx}", // Collect coverage from all JS and JSX files in the src folder
-    "!src/**/*.test.js", // Exclude test files (if necessary)
-    "!src/**/*.test.jsx", // Exclude test files (if necessary)
-  ],
+
 };
